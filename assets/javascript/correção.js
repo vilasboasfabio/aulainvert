2ea8bd25ee2bc7806a9b7f3004898528c7ccc88d
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-05-15 09:15:36 -0300

    mais um

--- assets/javascript/correção.js
+++ assets/javascript/correção.js
@@ -1,4 +1,4 @@
-function calculate(a,b){
+/*function calculate(a,b){
 
     return a + b;
 
@@ -8,5 +8,81 @@ let num1 = 5;
 let num2 = "10";
 
 let result = calculate(num1, Number(num2));
-console.log(result);
+console.log(result);*/
+
+/*function revert(str){
+
+    let reversed = "";
+
+    for ( let i = str.length - 1; i >=0 ; i--){
+        reversed += str[i];
+        
+    }
+
+    return reversed;
+ 
+}
+
+let string = "hello";
+let reversed = revert(string);
+console.log(reversed);*/
+
+/*function getMenor(numbers){
+
+    let menor = number[0]
+    
+    for (let i = 0; i < numbers.length; i++){
+        if(numbers[i]< menor){
+            
+            menor = numbers[i];
+        }
+
+    }
+    return menor;
+}
+
+numbers = [5, 10, 2, 8, 3];
+
+let menor = getMenor(numbers);
+console.log("O menor é: " + menor);*/
+
+/*function factorial(n){
+
+    if(n===0){
+        return 1;
+    }else if(n<0){
+        console.log("Sua anta! Fatorial começa no 0");
+    
+    }
+    else{
+        return n * factorial(n - 1);
+
+    }
+}
+
+let result = factorial(-6);
+console.log("O valor é: "  + result);*/
+
+var list =[];
+
+
+function adicionarEntrada(){
+    var entrada = document.getElementById("texto").value;
+
+    list.push(entrada);
+
+    document.getElementById("texto").value = '';
+     console.log(list);
+   
+     var conteudoArr = "";
+     for (let i = 0; i < list.length; i++){
+
+        conteudoArr += `<p class='estilo'>${list[i]}<p>`
+
+     }
+     
+     document.getElementById("result").innerHTML = conteudoArr;
+
+}
+
 
